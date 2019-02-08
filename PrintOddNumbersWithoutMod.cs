98ddc7fe5776bb4f072aa966bf3143a0e057c793
Devendra Milmile <d.h.milmile1@gmail.com>
2019-02-09 04:48:59 +0530

    PrintOddNumberWithoutMod.cs
    Printing the odd numbers from 1 to 100 without the % (mod) operator.

---
+++ PrintOddNumbersWithoutMod.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //Program to print odd numbers from 1 to 100 without %
+         for(int i=1;i<100;i+=2){
+         Console.WriteLine(i);
+         }
+        }
+     }
+}
